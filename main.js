2a78c7d8aebb95ad468055b9a7f615ba26df3a0a
bsc01191002 <66561807+bsc01191002@users.noreply.github.com>
2021-03-25 17:41:36 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    getProducts();
+    function getProducts() {
+      let url = "https://fakestoreapi.herokuapp.com/products";
+      let xhr = new XMLHttpRequest();
+      xhr.open(
+        "GET",
+        url,
+        true
+      );
+      xhr.onload = function() {
+        if(this.status == 200) {
+          let result = JSON.parse(this.responseText);
+          outputProducts(result);
+        }
+      }
+      xhr.send();
+    }
+  
+    function outputProducts(products) {
+      let productsdiv = document.querySelector(".products");
+      console.log(products);
+      let output = "";
+      for (let i = 0; i < 4; i++) {
+          output += `
+          <div class="col-md-3">
+              <img src="${products[i].image}" alt="">
+              <h3>${products[i].price}</h3>
+              <p>${products[i].title}</p>
+          </div>`;
+          
+      } 
+    
+      productsdiv.innerHTML = output;
+    }
+  })
+  
